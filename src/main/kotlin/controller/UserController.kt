82dbe72aaffa18361bb3377a32812c8c927cabfb
Drjacky <dr.jacky.2005@gmail.com>
2025-10-19 13:59:59 +0100

    [WIP] Get user by model

--- src/main/kotlin/controller/UserController.kt
+++ src/main/kotlin/controller/UserController.kt
@@ -1,5 +1,6 @@
 package app.web.drjacky.controller
 
+import app.web.drjacky.model.User
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,5 +9,8 @@ class UserController {
 
     @GetMapping("/getUser")
     fun getUser() = "Drjacky"
+
+    @GetMapping("/getUserByModel")
+    fun getUserByModel() = User(1, "Dr", "Jacky")
 
 }
---
+++ src/main/kotlin/model/User.kt
@@ -0,0 +1,7 @@
+package app.web.drjacky.model
+
+data class User(
+    val membershipId: Int,
+    val firstName: String,
+    val lastName: String,
+)
