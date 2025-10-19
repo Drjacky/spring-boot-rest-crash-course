c4432f5987a73b7cb6be2408b38104ad71685549
Drjacky <dr.jacky.2005@gmail.com>
2025-10-19 14:18:05 +0100

    [WIP] Get user by username

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "app.web.drjacky"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
-    implementation("org.springframework.boot:spring-boot-starter-web:3.5.6")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
 
-    testImplementation(kotlin("test"))
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        javaParameters.set(true)
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
--- src/main/kotlin/controller/UserController.kt
+++ src/main/kotlin/controller/UserController.kt
@@ -2,15 +2,24 @@ package app.web.drjacky.controller
 
 import app.web.drjacky.model.User
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserController {
+
+    private val userMap = HashMap<String, User>().also {
+        it["drjacky"] = User(1, "Dr", "Jacky")
+        it["blah"] = User(2, "Bl", "a")
+    }
 
     @GetMapping("/getUser")
     fun getUser() = "Drjacky"
 
     @GetMapping("/getUserByModel")
     fun getUserByModel() = User(1, "Dr", "Jacky")
+
+    @GetMapping("/getUserByUserName/{userName}")
+    fun getUserByUserName(@PathVariable userName: String) = userMap.get(userName)
 
 }
