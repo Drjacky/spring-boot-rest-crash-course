a55eae8af19f1e03651d16fbaa1e1482b80b613e
Drjacky <dr.jacky.2005@gmail.com>
2025-10-19 14:42:17 +0100

    Add user response http status

--- src/main/kotlin/controller/UserController.kt
+++ src/main/kotlin/controller/UserController.kt
@@ -1,6 +1,8 @@
 package app.web.drjacky.controller
 
 import app.web.drjacky.model.User
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -25,8 +27,9 @@ class UserController {
     fun getUserByUserName(@PathVariable userName: String) = userMap.get(userName)
 
     @PostMapping("/addUser")
-    fun addUser(@RequestBody user: User) {
+    fun addUser(@RequestBody user: User): ResponseEntity<HttpStatus> {
         userMap[user.firstName] = user
+        return ResponseEntity.accepted().build()
     }
 
 }
